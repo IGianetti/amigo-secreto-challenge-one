130c8d9ca4b0d184f5c7eeaf363fa1c6550975d8
Ivan Gianetti <88752023+IGianetti@users.noreply.github.com>
2025-02-14 15:59:39 -0300

    Agrega condiciones para iniciar y reiniciar el juego

--- app.js
+++ app.js
@@ -9,16 +9,16 @@ function agregarAmigo(){
     if (validarNombre(nombre)) {
         const nombreCapitalizado = capitalizarInicial(nombre)
         amigos.push(nombreCapitalizado);      // Agregamos el nombre a la lista  
-        input.value = "";         // limpiamos el campo de entrada 
-        console.log(amigos)
-        mostrarAmigos();
+        input.value = "";         // limpiamos el campo de entrada  
+        console.log(amigos)       
+        mostrarListaAmigos();
     } else {
         input.value = "";         // limpiamos el campo de entrada
     }
 }
 
 function validarNombre(nombre) {
-    // Convertimos a minusculs y eliminamos espacios
+    // Convertimos a minusculas y eliminamos espacios
     const nombreModelo = nombre.trim().toLowerCase();
     
     
@@ -53,9 +53,10 @@ function crearElemLista(nombreAmigo) {
     return nuevoElemLi;
 }
 
-function mostrarAmigos() {
+function mostrarListaAmigos() {
     const lista = document.getElementById("listaAmigos"); // Obtengo lista de amigos
     lista.innerHTML = ""; // Limpio lista de amigos
+    
 
     for (let i = 0; i < amigos.length; i++) {
         const nombreAmigo = amigos[i]; // Obtener amigo actual
@@ -67,15 +68,50 @@ function mostrarAmigos() {
 // funcion para sortear amigos
 //  KISS!!!
 
-function sortearAmigo() {
-    // Compruebo que la lista no este vacia. Si no esta vacia realizo el sorteo
-    if (amigos.length === 0) {
-        alert("Lista vacia, no hay amigos para sortear!")
-    } else {
+function sortearAmigo() {    
+
+        const listaSorteo = amigos
         const nroAleatorio = Math.floor(Math.random() * amigos.length)
         const amigoSecreto = amigos[nroAleatorio]
-    
+
+        listaSorteo.splice(nroAleatorio, 1); // Eliminamos al ya soretado
+        
         const mostrarAmigoSecreto = document.getElementById("resultado")
-        mostrarAmigoSecreto.innerHTML = `Tu amigo secreto sera: ${amigoSecreto}`}
+        if (amigoSecreto != undefined && amigoSecreto.length > 0) {
+            mostrarAmigoSecreto.innerHTML = `Tu amigo secreto sera: ${amigoSecreto}`
+            console.log(listaSorteo)
+        }     else {
+            reiniciar() // Si no hay mas amigos para sortear reinicio el juego
+        }    
+     
+     
+
 }
+
+function iniciarSorteo() {
+     // Compruebo que la lista no este vacia. Si no esta vacia realizo el sorteo
+     if (amigos.length === 0) {
+        alert("Lista vacia, no hay amigos para sortear!")
+        return false
+    } 
+    // Compruebo que almenos haya 2 amigos para realizar el sorteo, si es verdadero realizo el sorteo
+    if (amigos.length < 2) {
+        alert('Se necesitan minimo 2 amigos para realizar el sorteo!')
+        return true
+    } else {
+        sortearAmigo()
+    }
+}
+
+function reiniciar() {
+    const mostrarAmigoSecreto = document.getElementById("resultado")
+        mostrarAmigoSecreto.innerHTML = ""
+        const lista = document.getElementById("listaAmigos"); 
+        lista.innerHTML = ""    
+
+}
+
+
+
+
 
